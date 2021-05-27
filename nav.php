3d686853f1a1d7374598a7875c9f092c60d45320
mihikochan12 <mihikochan12@gmail.com>
2021-05-27 13:50:42 +0800

    Initial commit

---
+++ nav.php
@@ -0,0 +1,10 @@
+<h1>Police Emergency Service System</h1>
+<div id="HeaderStyle">
+<link rel="stylesheet" href="header_style.css">
+<nav>
+	<a href="logcall.php">Log Call</a>
+	<a href="update.php">Update</a>
+	<a href="#">Report</a>
+	<a href="#">History</a>
+</nav>
+</div>
